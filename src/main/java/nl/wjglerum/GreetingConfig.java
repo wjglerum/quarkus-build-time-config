f3da8ba5c6a47a558eebd7192f7fd0cbce2ef7ac
Willem Jan Glerum <wjglerum@gmail.com>
2021-05-20 13:01:16 +0200

    Provides a default value for the greeting config

--- src/main/java/nl/wjglerum/GreetingConfig.java
+++ src/main/java/nl/wjglerum/GreetingConfig.java
@@ -7,6 +7,6 @@ import io.quarkus.runtime.annotations.ConfigRoot;
 @ConfigRoot(name = "greeting", phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
 public class GreetingConfig {
 
-    @ConfigItem
+    @ConfigItem(defaultValue = "Quarkus")
     public String name;
 }
